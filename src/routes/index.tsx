74c41cec3a9effdf9f9195c381f428a4ab098ee5
kr4phy <kr4phy@gmail.com>
2026-02-22 12:23:27 +0900

    Initialize project with PWA setup, routing, and basic components

---
+++ src/routes/index.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute } from '@tanstack/react-router'
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+
+  const data = {
+    "date": "2026-02-22",
+    "explanation": "What if you saw your shadow on Mars and it wasn't human?  Then you might be the Opportunity rover exploring Mars.  Opportunity explored the Red Planet from 2004 to 2018, finding evidence of ancient water, and sending breathtaking images across the inner Solar System.  Pictured here in 2004, Opportunity looks opposite the Sun into Endurance Crater and sees its own shadow.  Two wheels are visible on the lower left and right, while the floor and walls of the unusual crater are visible in the background.  Caught in a dust storm in 2018, Opportunity stopped responding, and NASA stopped trying to contact it in 2019 and declared the ground-breaking mission, originally planned for only 92 days, complete.",
+    "hdurl": "https://apod.nasa.gov/apod/image/2602/marsshadow_opportunity_1024.jpg",
+    "media_type": "image",
+    "service_version": "v1",
+    "title": "Shadow of a Martian Robot",
+    "url": "https://apod.nasa.gov/apod/image/2602/marsshadow_opportunity_960.jpg"
+  }
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <div className="card card-border bg-base-80 w-196 shadow-sm">
+        <figure>
+          <img
+            src={data.url}
+            alt={data.title} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{data.title}</h2>
+          <p className='line-clamp-3'>{data.explanation}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    viteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icons.png', 'maskable-icons.png',],
+      manifest: {
+        name: 'Daily Astro Pictures',
+        short_name: 'DAP',
+        description: 'View and download daily astro pictures',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.origin === 'https://api.nasa.gov',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'nasa-api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+})
+
+export default config
