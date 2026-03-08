2c3e1b1f171ab5c3cd4a414e0d96720d939cc220
kr4phy <kr4phy@gmail.com>
2026-03-08 18:45:45 +0900

    Add API data fetching and storing methods and connect to UI.

---
+++ app/lib/api.ts
@@ -0,0 +1,90 @@
+type rawApodApiData = {
+    "date": string,
+    "explanation": string,
+    "hdurl": string,
+    "media_type": string,
+    "service_version": string,
+    "title": string,
+    "url": string
+}
+
+type apodData = {
+    "date": string,
+    "explanation": string,
+    "hdurl": string,
+    "media_type": string,
+    "service_version": string,
+    "title": string,
+    "url": string,
+    "encodedImage"?: string,
+    "isStarred": boolean
+}
+
+export const getAPODData = async (date: string): Promise<apodData> => {
+    if (import.meta.client) {
+        const stored = localStorage.getItem(`apod-${date}`);
+        if (stored) {
+            const localData = JSON.parse(stored);
+            if (localData.date === date) {
+                return localData;
+            }
+        }
+    }
+    const runtimeConfig = useRuntimeConfig();
+    const apiKey = runtimeConfig.public.apodApiKey;
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`);
+    const rawData: rawApodApiData = await response.json();
+    const data: apodData = { ...rawData, isStarred: false };
+    setAPODData(data);
+    return data;
+};
+
+export const setAPODData = (data: apodData): void => {
+    if (import.meta.client) {
+        const items = JSON.parse(localStorage.getItem("apod-items") || "[]");
+        if (!items.includes(data.date)) {
+            items.push(data.date);
+            items.sort();
+            localStorage.setItem("apod-items", JSON.stringify(items));
+        }
+        if(data.isStarred) {
+            const starredItems = JSON.parse(localStorage.getItem("apod-starred-items") || "[]");
+            if (!starredItems.includes(data.date)) {
+                starredItems.push(data.date);
+                starredItems.sort();
+                localStorage.setItem("apod-starred-items", JSON.stringify(starredItems));
+            }
+        } else {
+            if(JSON.parse(localStorage.getItem("apod-starred-items") || "[]").includes(data.date)) {
+                const starredItems = JSON.parse(localStorage.getItem("apod-starred-items") || "[]").filter((item: string) => item !== data.date);
+                localStorage.setItem("apod-starred-items", JSON.stringify(starredItems));
+            }
+        }
+        if(data.encodedImage) {
+            const downloadedItems = JSON.parse(localStorage.getItem("apod-downloaded-items") || "[]");
+            if (!downloadedItems.includes(data.date)) {
+                downloadedItems.push(data.date);
+                downloadedItems.sort();
+                localStorage.setItem("apod-downloaded-items", JSON.stringify(downloadedItems));
+            }
+        }
+        localStorage.setItem(`apod-${data.date}`, JSON.stringify(data));
+    }
+}
+
+export const getCurrentDate = (): string => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const date = `${year}-${month}-${day}`;
+    return date;
+}
+
+export async function downloadImage(date: string) {
+    const data = await getAPODData(date);
+    const buffer = await $fetch<ArrayBuffer>(data.hdurl, { responseType: 'arrayBuffer' });
+    const encodedImage = window.btoa(String.fromCharCode(...new Uint8Array(buffer)));
+
+    return encodedImage;
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,35 +1,43 @@
 <script setup lang="ts">
-const data = {
-    "date": "2026-02-22",
-    "explanation": "What if you saw your shadow on Mars and it wasn't human?  Then you might be the Opportunity rover exploring Mars.  Opportunity explored the Red Planet from 2004 to 2018, finding evidence of ancient water, and sending breathtaking images across the inner Solar System.  Pictured here in 2004, Opportunity looks opposite the Sun into Endurance Crater and sees its own shadow.  Two wheels are visible on the lower left and right, while the floor and walls of the unusual crater are visible in the background.  Caught in a dust storm in 2018, Opportunity stopped responding, and NASA stopped trying to contact it in 2019 and declared the ground-breaking mission, originally planned for only 92 days, complete.",
-    "hdurl": "https://apod.nasa.gov/apod/image/2602/marsshadow_opportunity_1024.jpg",
-    "media_type": "image",
-    "service_version": "v1",
-    "title": "Shadow of a Martian Robot",
-    "url": "https://apod.nasa.gov/apod/image/2602/marsshadow_opportunity_960.jpg"
-}
-const isStarred = ref(false)
+import { getAPODData, getCurrentDate, setAPODData } from '~/lib/api';
+
+const data = await getAPODData(getCurrentDate())
+
+const isStarred = ref(!!data.isStarred);
+const imageLoading = ref(true);
+
+watch(isStarred, (newValue) => {
+    data.isStarred = newValue;
+    setAPODData(data);
+});
+
 </script>
 <template>
     <UPage>
         <UPageHeader title="Today's APOD" />
         <UPageBody>
-            <UPageCard orientation="vertical" reverse>
-                <template #title>{{ data.title }}</template>
-                <template #description class="">{{ data.explanation }}</template>
-                <img :src="data.url" :alt="data.title" class="w-full rounded-lg" />
-                <template #footer>
-                    <div class="flex justify-end gap-2">
-                        <div>
+            <ClientOnly>
+                <UCard>
+                    <template #header>
+                        <h2 class="text-xl font-semibold">{{ data.title }}</h2>
+                    </template>
+                    <div class="space-y-4">
+                        <div class="w-full aspect-video bg-black rounded-lg relative">
+                            <USkeleton v-show="imageLoading" class="w-full h-full absolute inset-0" />
+                            <img :src="data.url" :alt="data.title" class="w-full h-full object-contain rounded-lg" 
+                                @load="imageLoading = false" v-show="!imageLoading" />
+                        </div>
+                        <p class="text-sm text-gray-600 dark:text-gray-400">{{ data.explanation }}</p>
+                    </div>
+                    <template #footer>
+                        <div class="flex justify-end gap-2">
                             <UButton :icon="isStarred ? 'i-heroicons-star-solid' : 'i-heroicons-star'" size="md"
                                 color="neutral" variant="ghost" @click="isStarred = !isStarred" />
-                        </div>
-                        <div>
                             <UButton icon="i-heroicons-folder-arrow-down" size="md" color="neutral" variant="ghost" />
                         </div>
-                    </div>
-                </template>
-            </UPageCard>
+                    </template>
+                </UCard>
+            </ClientOnly>
         </UPageBody>
     </UPage>
 </template>
